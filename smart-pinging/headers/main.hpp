f9f2931f46858d52c0b5123b1d040d0c5d22ef32
atomlin <atomlin.main@gmail.com>
2023-10-27 18:02:57 +0300

    Добавил файлы

---
+++ smart-pinging/headers/main.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <memory.h>
+#include <float.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <io.h>
+
+#include <winsock.h>
+#pragma comment (lib, "ws2_32.lib")
+
+#pragma warning(disable:4067)
+#pragma warning(disable:4127)
+#pragma warning(disable:4309)
+#pragma warning(disable:4838)
+#pragma warning(disable:4996)
+#pragma warning(disable:6054)
